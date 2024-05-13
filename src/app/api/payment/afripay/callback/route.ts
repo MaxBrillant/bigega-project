13ea10d19f719482704a5c4a5f7a31edb5591769
Max <ndashimax37@gmail.com>
2024-05-13 18:45:54 +0300

    Fixing the webhook

--- src/app/api/payment/afripay/callback/route.ts
+++ src/app/api/payment/afripay/callback/route.ts
@@ -2,8 +2,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
+  console.log("The webhook has been received");
+  const body = request.json();
   console.log(body);
+  console.log(request.body);
+  console.log("The webhook has been used");
   // const status: string = body.status;
   // const amount: string = body.amount;
   // const currency: string = body.currency;
@@ -11,5 +14,5 @@ export async function POST(request: NextRequest) {
   // const paymentMethod: "ECOCASH" | "LUMICASH" = body.payment_method;
   // const donation_id: string = body.donation_id;
 
-  return new NextResponse();
+  return NextResponse.redirect("www.bigega.com");
 }
